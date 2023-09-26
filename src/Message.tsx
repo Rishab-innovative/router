d065a9ff6e15f77376ae925646777b3ccdbfe9c5
Rishab-innovative <rishab.kumar@innotechteam.in>
2023-09-26 19:11:01 +0530

    working on router

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+const Message: React.FC = () => {
+  return (
+    <div className="container">
+      <h2>THIS IS MESSAGE PAGE</h2>
+      <h4>message number 1</h4>
+      <h4>message number 2</h4>
+      <h4>message number 3</h4>
+    </div>
+  );
+};
+export default Message;
